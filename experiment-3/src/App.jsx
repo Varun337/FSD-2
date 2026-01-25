a7acf9cb5fa8902a3a234a9909fac0b3fd57aa6b
Varun337 <varunparmar7689@gmail.com>
2026-01-25 21:44:40 +0530

    Added experiment-3 (Vite React SPA routing)

---
+++ experiment-3/src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Contact from "./pages/Contact.jsx";
+
+
+function App() {
+  return (
+   <BrowserRouter>
+  <div className="app-container">
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/contact">Contact</Link>
+    </nav>
+
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  </div>
+</BrowserRouter>
+
+  );
+}
+
+export default App;
